9dd13e0b578880afef45cde95acdd8dcb1561193
CapuchaRojo <121252498+CapuchaRojo@users.noreply.github.com>
2025-02-22 20:27:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import json
 import requests
-from collections import defaultdict
+from collections import defaultdict  
+from flask import Flask
+  from dotenv import load_dotenv
+  import os
+
+  load_dotenv()
+
+  app = Flask(__name__)
+  app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
 
 class GoalBasedIncidentAgent:
     def __init__(self, knowledge_base, granite_api_key):
